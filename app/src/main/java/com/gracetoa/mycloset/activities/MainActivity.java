dadd17afde60896dd2a010abb8541ca9bc0f8520
GraceToa <gracetoa29@gmail.com>
2019-11-27 23:10:16 +0100

    creating floatingActionBarMenu

--- app/src/main/java/com/gracetoa/mycloset/activities/MainActivity.java
+++ app/src/main/java/com/gracetoa/mycloset/activities/MainActivity.java
@@ -2,14 +2,24 @@ package com.gracetoa.mycloset.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
+
+import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.gracetoa.mycloset.R;
 import com.gracetoa.mycloset.adapters.PagerAdapter;
+import com.gracetoa.mycloset.fragments.AddEditClotheFragment;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        final FloatingActionButton fab1 = findViewById(R.id.fab1);
+        final FloatingActionButton fab2 = findViewById(R.id.fab2);
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         TextView customTitleToolbar = toolbar.findViewById(R.id.textTitleToolbar);
@@ -51,5 +64,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
+
+        fab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Outfit ADD", Toast.LENGTH_LONG).show();
+
+                startActivity(new Intent(MainActivity.this,AddEditClotheActivity.class));
+            }
+        });
+
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Clothe ADD", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    public void replaceFragment(){
+        AddEditClotheFragment addEditClotheFragment = new AddEditClotheFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container,addEditClotheFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
